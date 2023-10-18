6b6e8237cfbb5c6010a0033da007258b1976cbda
naylamreina <nreina@itba.edu.ar>
2023-10-18 06:35:55 +0000

    exp031

--- src/workflow-semillerio/exp031/701_RUN_correr_workflow_031.r
+++ src/workflow-semillerio/exp031/701_RUN_correr_workflow_031.r
@@ -10,11 +10,11 @@ options(error = function() {
 # corrida de cada paso del workflow
 
 # primeros pasos, relativamente rapidos
-source("~/dm2023b/src/workflow-semillerio/exp029/711_CA_reparar_dataset_029.r")
-source("~/dm2023b/src/workflow-semillerio/exp029/721_DR_corregir_drifting_029.r")
-source("~/dm2023b/src/workflow-semillerio/exp029/731_FE_historia_029.r")
-source("~/dm2023b/src/workflow-semillerio/exp029/741_TS_training_strategy_029.r")
+source("~/dm2023b/src/workflow-semillerio/exp031/711_CA_reparar_dataset_031.r")
+source("~/dm2023b/src/workflow-semillerio/exp031/721_DR_corregir_drifting_031.r")
+source("~/dm2023b/src/workflow-semillerio/exp031/731_FE_historia_031.r")
+source("~/dm2023b/src/workflow-semillerio/exp031/741_TS_training_strategy_031.r")
 
 # ultimos pasos, muy lentos
-source("~/dm2023b/src/workflow-semillerio/exp029/751_HT_lightgbm_029.r")
-source("~/dm2023b/src/workflow-semillerio/exp029/795_ZZ_final_semillerio_029.r")
+source("~/dm2023b/src/workflow-semillerio/exp031/751_HT_lightgbm_031.r")
+source("~/dm2023b/src/workflow-semillerio/exp031/795_ZZ_final_semillerio_031.r")
